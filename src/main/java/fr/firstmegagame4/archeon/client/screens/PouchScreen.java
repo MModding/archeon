aae4e995d6190f54e65b6ba64e6053254d5087de
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2023-08-16 02:30:47 +0200

    Fix Pouch Screen

--- src/main/java/fr/firstmegagame4/archeon/client/screens/PouchScreen.java
+++ src/main/java/fr/firstmegagame4/archeon/client/screens/PouchScreen.java
@@ -1,6 +1,7 @@
 package fr.firstmegagame4.archeon.client.screens;
 
 
+import com.mmodding.mmodding_lib.library.utils.Colors;
 import com.mojang.blaze3d.systems.RenderSystem;
 import fr.firstmegagame4.archeon.Archeon;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
@@ -12,30 +13,31 @@ import net.minecraft.text.Text;
 
 public class PouchScreen extends HandledScreen<ScreenHandler> {
 
-    public PouchScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
-        super(handler, inventory, title);
-    }
+	public PouchScreen(ScreenHandler handler, PlayerInventory inventory, Text title) {
+		super(handler, inventory, title);
+		this.backgroundWidth = 191;
+		this.backgroundHeight = 176;
+	}
 
-    @Override
-    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-        RenderSystem.setShaderTexture(0, Archeon.createId("textures/gui/pouch.png"));
-        int x = (this.width - this.backgroundWidth) / 2;
-        int y = (this.height - this.backgroundHeight) / 2;
-        this.drawTexture(matrices, x, y, 0, 0, this.backgroundWidth, this.backgroundHeight);
-    }
+	@Override
+	protected void drawForeground(MatrixStack matrices, int mouseX, int mouseY) {
+		this.textRenderer.draw(matrices, this.title, this.titleX, this.titleY, new Colors.RGB(136, 75, 43).toDecimal());
+	}
 
-    @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
-        this.drawMouseoverTooltip(matrices, mouseX, mouseY);
-    }
+	@Override
+	protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+		RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+		RenderSystem.setShaderTexture(0, Archeon.createId("textures/gui/pouch.png"));
+		int x = (this.width - this.backgroundWidth) / 2 - 8;
+		int y = (this.height - this.backgroundHeight) / 2 - 9;
+		this.drawTexture(matrices, x, y, 0, 0, this.backgroundWidth, this.backgroundHeight);
+	}
 
-    @Override
-    protected void init() {
-        super.init();
-        this.titleX = (this.backgroundWidth - this.textRenderer.getWidth(this.title)) / 2;
-    }
+	@Override
+	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+		this.renderBackground(matrices);
+		super.render(matrices, mouseX, mouseY, delta);
+		this.drawMouseoverTooltip(matrices, mouseX, mouseY);
+	}
 }
