999cdea2a511f0f6673bf325ec802398625c6088
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2025-02-21 15:50:58 +0100

    Change HeiferEntityModel

--- src/main/java/com/mmodding/archeon/client/entities/models/AuroraCatalystEntityModel.java
+++ src/main/java/com/mmodding/archeon/client/entities/models/AuroraCatalystEntityModel.java
@@ -30,86 +30,86 @@ public class AuroraCatalystEntityModel extends EntityModel<AuroraCatalystEntity>
 		ModelPartData bone = modelPartData.addChild(
 			EntityModelPartNames.BONE,
 			ModelPartBuilder.create(),
-			ModelTransform.pivot(0.0F, 24.0F, 0.0F)
+			ModelTransform.pivot(0.0f, 24.0f, 0.0f)
 		);
 
 		bone.addChild(
 			"center",
 			ModelPartBuilder.create()
 				.uv(0, 0)
-				.cuboid(-1.0F, -8.0F, -1.0F, 2.0F, 16.0F, 2.0F, new Dilation(0.0F)),
-			ModelTransform.pivot(0.0F, -10.0F, 0.0F)
+				.cuboid(-1.0f, -8.0f, -1.0f, 2.0f, 16.0f, 2.0f, new Dilation(0.0f)),
+			ModelTransform.pivot(0.0f, -10.0f, 0.0f)
 		);
 
 		ModelPartData upper_shields = bone.addChild(
 			"upper_shields",
 			ModelPartBuilder.create(),
-			ModelTransform.pivot(0.0F, -15.0F, 0.0F)
+			ModelTransform.pivot(0.0f, -15.0f, 0.0f)
 		);
 
 		ModelPartData shields3 = upper_shields.addChild(
 			"shields3",
 			ModelPartBuilder.create()
 				.uv(26, 13)
-				.cuboid(1.0F, -8.0F, -1.0F, 1.0F, 4.0F, 2.0F, new Dilation(0.0F))
+				.cuboid(1.0f, -8.0f, -1.0f, 1.0f, 4.0f, 2.0f, new Dilation(0.0f))
 				.uv(22, 9)
-				.cuboid(-2.0F, -8.0F, -1.0F, 1.0F, 4.0F, 2.0F, new Dilation(0.0F)),
-			ModelTransform.of(0.0F, 7.0F, 0.0F, 0.0F, -1.5708F, 0.0F)
+				.cuboid(-2.0f, -8.0f, -1.0f, 1.0f, 4.0f, 2.0f, new Dilation(0.0f)),
+			ModelTransform.of(0.0f, 7.0f, 0.0f, 0.0f, -1.5708f, 0.0f)
 		);
 
 		shields3.addChild(
 			"cube_r1",
 			ModelPartBuilder.create()
 				.uv(22, 20)
-				.cuboid(1.0F, -8.0F, -1.0F, 1.0F, 4.0F, 2.0F, new Dilation(0.0F))
+				.cuboid(1.0f, -8.0f, -1.0f, 1.0f, 4.0f, 2.0f, new Dilation(0.0f))
 				.uv(22, 26)
-				.cuboid(-2.0F, -8.0F, -1.0F, 1.0F, 4.0F, 2.0F, new Dilation(0.0F)),
-			ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 1.5708F, 0.0F)
+				.cuboid(-2.0f, -8.0f, -1.0f, 1.0f, 4.0f, 2.0f, new Dilation(0.0f)),
+			ModelTransform.of(0.0f, 0.0f, 0.0f, 0.0f, 1.5708f, 0.0f)
 		);
 
 		ModelPartData bottom_shields = bone.addChild(
 			"bottom_shields",
 			ModelPartBuilder.create(),
-			ModelTransform.pivot(0.0F, -11.0F, 0.0F)
+			ModelTransform.pivot(0.0f, -11.0f, 0.0f)
 		);
 
 		ModelPartData shields1 = bottom_shields.addChild(
 			"shields1",
 			ModelPartBuilder.create()
 				.uv(6, 16)
-				.cuboid(2.0F, -15.0F, -1.0F, 1.0F, 11.0F, 2.0F, new Dilation(0.0F))
+				.cuboid(2.0f, -15.0f, -1.0f, 1.0f, 11.0f, 2.0f, new Dilation(0.0f))
 				.uv(16, 0)
-				.cuboid(-3.0F, -15.0F, -1.0F, 1.0F, 11.0F, 2.0F, new Dilation(0.0F)),
-			ModelTransform.of(0.0F, 11.0F, 0.0F, 0.0F, -0.7854F, 0.0F)
+				.cuboid(-3.0f, -15.0f, -1.0f, 1.0f, 11.0f, 2.0f, new Dilation(0.0f)),
+			ModelTransform.of(0.0f, 11.0f, 0.0f, 0.0f, -0.7854f, 0.0f)
 		);
 
 		shields1.addChild(
 			"cube_r2",
 			ModelPartBuilder.create()
 				.uv(8, 0)
-				.cuboid(2.0F, -14.0F, -1.0F, 1.0F, 11.0F, 2.0F, new Dilation(0.0F))
+				.cuboid(2.0f, -14.0f, -1.0f, 1.0f, 11.0f, 2.0f, new Dilation(0.0f))
 				.uv(12, 11)
-				.cuboid(-3.0F, -14.0F, -1.0F, 1.0F, 11.0F, 2.0F, new Dilation(0.0F)),
-			ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 1.5708F, 0.0F)
+				.cuboid(-3.0f, -14.0f, -1.0f, 1.0f, 11.0f, 2.0f, new Dilation(0.0f)),
+			ModelTransform.of(0.0f, 0.0f, 0.0f, 0.0f, 1.5708f, 0.0f)
 		);
 
 		ModelPartData shields2 = bottom_shields.addChild(
 			"shields2", ModelPartBuilder.create()
 				.uv(22, 0)
-				.cuboid(2.0F, -11.0F, -1.0F, 1.0F, 7.0F, 2.0F, new Dilation(0.0F))
+				.cuboid(2.0f, -11.0f, -1.0f, 1.0f, 7.0f, 2.0f, new Dilation(0.0f))
 				.uv(0, 18)
-				.cuboid(-3.0F, -11.0F, -1.0F, 1.0F, 7.0F, 2.0F, new Dilation(0.0F)),
-			ModelTransform.of(0.0F, 9.0F, 0.0F, 0.0F, -1.5708F, 0.0F)
+				.cuboid(-3.0f, -11.0f, -1.0f, 1.0f, 7.0f, 2.0f, new Dilation(0.0f)),
+			ModelTransform.of(0.0f, 9.0f, 0.0f, 0.0f, -1.5708f, 0.0f)
 		);
 
 		shields2.addChild(
 			"cube_r3",
 			ModelPartBuilder.create()
 				.uv(18, 13)
-				.cuboid(2.0F, -11.0F, -1.0F, 1.0F, 7.0F, 2.0F, new Dilation(0.0F))
+				.cuboid(2.0f, -11.0f, -1.0f, 1.0f, 7.0f, 2.0f, new Dilation(0.0f))
 				.uv(16, 22)
-				.cuboid(-3.0F, -11.0F, -1.0F, 1.0F, 7.0F, 2.0F, new Dilation(0.0F)),
-			ModelTransform.of(0.0F, 0.0F, 0.0F, 0.0F, 1.5708F, 0.0F)
+				.cuboid(-3.0f, -11.0f, -1.0f, 1.0f, 7.0f, 2.0f, new Dilation(0.0f)),
+			ModelTransform.of(0.0f, 0.0f, 0.0f, 0.0f, 1.5708f, 0.0f)
 		);
 
 		return TexturedModelData.of(modelData, 32, 32);
--- src/main/java/com/mmodding/archeon/client/entities/models/HeiferEntityModel.java
+++ src/main/java/com/mmodding/archeon/client/entities/models/HeiferEntityModel.java
@@ -1,22 +1,41 @@
 package com.mmodding.archeon.client.entities.models;
 
 import com.mmodding.archeon.entities.HeiferEntity;
-import net.minecraft.client.model.ModelPart;
-import net.minecraft.client.model.TexturedModelData;
-import net.minecraft.client.render.entity.model.CowEntityModel;
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.entity.model.EntityModelPartNames;
+import net.minecraft.client.render.entity.model.QuadrupedEntityModel;
 
-public class HeiferEntityModel extends CowEntityModel<HeiferEntity> {
+// Originally Made with Blockbench 4.9.0
+@SuppressWarnings("unused")
+public class HeiferEntityModel extends QuadrupedEntityModel<HeiferEntity> {
 
 	public HeiferEntityModel(ModelPart root) {
-		super(root);
+		super(root, false, 10.0f, 4.0f, 2.0f, 2.0f, 24);
 	}
 
 	public static TexturedModelData getTexturedModelData() {
-		return CowEntityModel.getTexturedModelData();
-	}
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
 
-	@Override
-	public void setAngles(HeiferEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-		super.setAngles(entity, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
+		ModelPartData body = modelPartData.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(0, 0).cuboid(-6.0f, -7.5f, -9.75f, 12.0f, 13.0f, 20.0f, new Dilation(0.0f))
+			.uv(64, 21).cuboid(-6.0f, 5.5f, -9.75f, 12.0f, 3.0f, 0.0f, new Dilation(0.0f))
+			.uv(64, 24).cuboid(-6.0f, 5.5f, 10.26f, 12.0f, 3.0f, 0.0f, new Dilation(0.0f))
+			.uv(0, 33).cuboid(6.0f, 5.5f, -9.75f, 0.0f, 3.0f, 20.0f, new Dilation(0.0f))
+			.uv(40, 33).cuboid(-6.0f, 5.5f, -9.75f, 0.0f, 3.0f, 20.0f, new Dilation(0.0f))
+			.uv(64, 14).cuboid(-2.0f, 5.5f, 4.25f, 4.0f, 1.0f, 6.0f, new Dilation(0.0f)), ModelTransform.pivot(0.0f, 8.5f, 0.75f));
+
+		ModelPartData head = modelPartData.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(0, 56).cuboid(-4.0f, -4.0f, -6.0f, 8.0f, 10.0f, 6.0f, new Dilation(0.0f))
+			.uv(64, 27).cuboid(-3.0f, 1.0f, -7.0f, 6.0f, 4.0f, 1.0f, new Dilation(0.0f))
+			.uv(28, 70).cuboid(5.0f, -7.0f, -5.0f, 2.0f, 5.0f, 2.0f, new Dilation(0.0f))
+			.uv(44, 70).cuboid(4.0f, -4.0f, -5.0f, 1.0f, 2.0f, 2.0f, new Dilation(0.0f))
+			.uv(50, 70).cuboid(-5.0f, -4.0f, -5.0f, 1.0f, 2.0f, 2.0f, new Dilation(0.0f))
+			.uv(36, 70).cuboid(-7.0f, -7.0f, -5.0f, 2.0f, 5.0f, 2.0f, new Dilation(0.0f)), ModelTransform.pivot(0.0f, 4.0f, -8.0f));
+
+		ModelPartData leftFrontLeg = modelPartData.addChild(EntityModelPartNames.LEFT_FRONT_LEG, ModelPartBuilder.create().uv(60, 56).cuboid(-2.01f, 0.0f, -2.0f, 4.0f, 10.0f, 4.0f, new Dilation(0.0f)), ModelTransform.pivot(4.0f, 14.0f, -5.0f));
+		ModelPartData rightFrontLeg = modelPartData.addChild(EntityModelPartNames.RIGHT_FRONT_LEG, ModelPartBuilder.create().uv(64, 0).cuboid(-1.99f, 0.0f, -2.0f, 4.0f, 10.0f, 4.0f, new Dilation(0.0f)), ModelTransform.pivot(-4.0f, 14.0f, -5.0f));
+		ModelPartData leftHindLeg = modelPartData.addChild(EntityModelPartNames.LEFT_HIND_LEG, ModelPartBuilder.create().uv(28, 56).cuboid(-2.01f, 0.0f, -2.01f, 4.0f, 10.0f, 4.0f, new Dilation(0.0f)), ModelTransform.pivot(4.0f, 14.0f, 8.0f));
+		ModelPartData rightHindLeg = modelPartData.addChild(EntityModelPartNames.RIGHT_HIND_LEG, ModelPartBuilder.create().uv(44, 56).cuboid(-1.99f, 0.0f, -2.01f, 4.0f, 10.0f, 4.0f, new Dilation(0.0f)), ModelTransform.pivot(-4.0f, 14.0f, 8.0f));
+
+		return TexturedModelData.of(modelData, 128, 128);
 	}
 }
