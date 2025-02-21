cbbfe8d8b4460af51958b4f3879d722d5e78cc63
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2025-02-21 21:39:44 +0100

    Make Criadan Turn Into Ceramic In Dascium After 30 Seconds

---
+++ src/main/java/com/mmodding/archeon/mixin/ItemEntityMixin.java
@@ -0,0 +1,48 @@
+package com.mmodding.archeon.mixin;
+
+import com.mmodding.archeon.init.ArcheonBlocks;
+import com.mmodding.archeon.init.ArcheonFluids;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemEntity.class)
+public abstract class ItemEntityMixin {
+
+	@Shadow
+	private int itemAge;
+
+	@Shadow
+	public abstract ItemStack getStack();
+
+	@Inject(method = "tick", at = @At("TAIL"))
+	private void ceramicCreation(CallbackInfo ci) {
+		ItemEntity itemEntity = ((ItemEntity) (Object) this);
+		if (!itemEntity.getWorld().isClient()) {
+			if (this.getStack().isOf(ArcheonBlocks.CRIADAN.getItem())) {
+				if (itemEntity.getWorld().getFluidState(itemEntity.getBlockPos()).isOf(ArcheonFluids.DASCIUM)) {
+					if (this.itemAge >= 600) {
+						int count = itemEntity.getStack().getCount();
+						double x = itemEntity.getX();
+						double y = itemEntity.getY();
+						double z = itemEntity.getZ();
+						double velocityX = itemEntity.getVelocity().x;
+						double velocityY = itemEntity.getVelocity().y;
+						double velocityZ = itemEntity.getVelocity().z;
+						itemEntity.discard();
+						itemEntity.world.spawnEntity(new ItemEntity(
+							itemEntity.world,
+							x, y, z,
+							new ItemStack(ArcheonBlocks.CERAMIC_BLOCK.getItem(), count),
+							velocityX, velocityY, velocityZ
+						));
+					}
+				}
+			}
+		}
+	}
+}
