b3421d31b67abc0e40ebe12ba9bfb26d1500ba16
FirstMegaGame4 <firstmegagame4@outlook.fr>
2026-03-30 21:33:26 +0200

    Add ArcheonPlacedFeatures

---
+++ src/main/java/com/mmodding/archeon/init/ArcheonPlacedFeatures.java
@@ -0,0 +1,97 @@
+package com.mmodding.archeon.init;
+
+import com.mmodding.archeon.Archeon;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.world.gen.feature.PlacedFeature;
+
+public class ArcheonPlacedFeatures {
+
+	public static final RegistryKey<PlacedFeature> PALM_TREE = of("palm_tree");
+	public static final RegistryKey<PlacedFeature> NECLANE_TREE = of("neclane_tree");
+	public static final RegistryKey<PlacedFeature> ROCKY_FIELDS_NECLANE_TREE = of("rocky_fields_neclane_tree");
+	public static final RegistryKey<PlacedFeature> CYPRESS_TREE = of("cypress_tree");
+	public static final RegistryKey<PlacedFeature> PNEVENTIAL_VUXANCIA_TREE = of("pnevential_vuxancia_tree");
+	public static final RegistryKey<PlacedFeature> STREIAN_VUXANCIA_TREE = of("streian_vuxancia_tree");
+	public static final RegistryKey<PlacedFeature> ORIAN_VUXANCIA_TREE = of("orian_vuxancia_tree");
+	public static final RegistryKey<PlacedFeature> VALE_VUXANCIA_TREE = of("vale_vuxancia_tree");
+	public static final RegistryKey<PlacedFeature> ZIAL_VUXANCIA_TREE = of("zial_vuxancia_tree");
+
+	public static final RegistryKey<PlacedFeature> PATCH_PNEVENTIAL_VUXANCIA_LEAVES = of("patch_pnevential_vuxancia_leaves");
+	public static final RegistryKey<PlacedFeature> PATCH_STREIAN_VUXANCIA_LEAVES = of("patch_streian_vuxancia_leaves");
+	public static final RegistryKey<PlacedFeature> PATCH_ORIAN_VUXANCIA_LEAVES = of("patch_orian_vuxancia_leaves");
+	public static final RegistryKey<PlacedFeature> PATCH_VALE_VUXANCIA_LEAVES = of("patch_vale_vuxancia_leaves");
+	public static final RegistryKey<PlacedFeature> PATCH_ZIAL_VUXANCIA_LEAVES = of("patch_zial_vuxancia_leaves");
+
+	public static final RegistryKey<PlacedFeature> VUXANCIA_POUND = of("vuxancia_pound");
+
+	public static final RegistryKey<PlacedFeature> PATCH_WET_GRASS = of("patch_wet_grass");
+	public static final RegistryKey<PlacedFeature> PATCH_TALL_WET_GRASS = of("patch_tall_wet_grass");
+	public static final RegistryKey<PlacedFeature> PATCH_FLOWERED_WET_GRASS = of("patch_flowered_wet_grass");
+	public static final RegistryKey<PlacedFeature> PATCH_FLOWERED_TALL_WET_GRASS = of("patch_flowered_tall_wet_grass");
+	public static final RegistryKey<PlacedFeature> COLD_PATCH_FLOWERED_WET_GRASS = of("cold_patch_flowered_wet_grass");
+	public static final RegistryKey<PlacedFeature> COLD_PATCH_FLOWERED_TALL_WET_GRASS = of("cold_patch_flowered_tall_wet_grass");
+	public static final RegistryKey<PlacedFeature> PATCH_WET_FERN = of("patch_wet_ferm");
+	public static final RegistryKey<PlacedFeature> PATCH_TALL_WET_FERN = of("patch_tall_wet_fern");
+	public static final RegistryKey<PlacedFeature> PATCH_WET_GRASS_TUFFET = of("patch_wet_grass_tuffet");
+	public static final RegistryKey<PlacedFeature> PATCH_SNOWY_GRASS_TUFFET = of("patch_snowy_grass_tuffet");
+	public static final RegistryKey<PlacedFeature> FOREST_PATCH_SNOWY_GRASS_TUFFET = of("forest_patch_snowy_grass_tuffet");
+
+	public static final RegistryKey<PlacedFeature> PATCH_SOUTH_WHEAT = of("patch_south_wheat");
+	public static final RegistryKey<PlacedFeature> PATCH_BUSH = of("patch_bush");
+	public static final RegistryKey<PlacedFeature> PATCH_VINE = of("patch_vine");
+	public static final RegistryKey<PlacedFeature> PATCH_BLOOD_ORANGE_BUSH = of("patch_blood_orange_bush");
+
+	public static final RegistryKey<PlacedFeature> PEAKS_SNOW = of("peaks_snow");
+
+	public static final RegistryKey<PlacedFeature> PATCH_SUNSET_ORCHID = of("patch_sunset_orchid");
+	public static final RegistryKey<PlacedFeature> PATCH_ROSEYPIA = of("patch_roseypia");
+	public static final RegistryKey<PlacedFeature> PATCH_AEROLIA = of("patch_aerolia");
+	public static final RegistryKey<PlacedFeature> PATCH_ASTEDIBES = of("patch_astedibes");
+	public static final RegistryKey<PlacedFeature> PATCH_TWILIGHT_ROSE = of("patch_twilight_rose");
+	public static final RegistryKey<PlacedFeature> PATCH_PINK_LYCORIS = of("patch_pink_lycoris");
+	public static final RegistryKey<PlacedFeature> PATCH_RED_LYCORIS = of("patch_red_lycoris");
+	public static final RegistryKey<PlacedFeature> PATCH_ORANGE_LYCORIS = of("patch_orange_lycoris");
+	public static final RegistryKey<PlacedFeature> PATCH_ASCENDIA = of("patch_ascendia");
+	public static final RegistryKey<PlacedFeature> PATCH_OEA = of("patch_oea");
+	public static final RegistryKey<PlacedFeature> PATCH_IOPEA = of("patch_iopea");
+	public static final RegistryKey<PlacedFeature> PATCH_ORGANDEANE = of("patch_organdeane");
+	public static final RegistryKey<PlacedFeature> PATCH_SEPTICEOLIA = of("patch_septiceolia");
+	public static final RegistryKey<PlacedFeature> PATCH_WHITE_DOELDIA = of("patch_white_doeldia");
+	public static final RegistryKey<PlacedFeature> PATCH_YELLOW_DOELDIA = of("patch_yellow_doeldia");
+	public static final RegistryKey<PlacedFeature> PATCH_XETULIAN_SUGAR_CANE = of("patch_xetulian_sugar_cane");
+
+	public static final RegistryKey<PlacedFeature> ORE_ARTEMNITE = of("ore_artemnite");
+	public static final RegistryKey<PlacedFeature> ORE_CRIADAN = of("ore_criadan");
+	public static final RegistryKey<PlacedFeature> ORE_AKETITE = of("ore_aketite");
+	public static final RegistryKey<PlacedFeature> ORE_SALT = of("ore_salt");
+	public static final RegistryKey<PlacedFeature> ORE_SHELLSAND = of("ore_shellsand");
+
+	public static final RegistryKey<PlacedFeature> ORE_APAFLORITE = of("ore_apaflorite");
+	public static final RegistryKey<PlacedFeature> ORE_EXYRIANE = of("ore_exyriane");;
+	public static final RegistryKey<PlacedFeature> ORE_FAELITE = of("ore_faelite");
+	public static final RegistryKey<PlacedFeature> ORE_LUSONYTH = of("ore_lusonyth");
+
+	public static final RegistryKey<PlacedFeature> GLOWSHROOMS_POOL = of("glowshrooms_pool");
+	public static final RegistryKey<PlacedFeature> GLOWSHROOMS_PATCH = of("glowshrooms_patch");
+	public static final RegistryKey<PlacedFeature> GLOWSHROOMS = of("glowshrooms");
+
+	public static final RegistryKey<PlacedFeature> CRIADAN_CLUSTER = of("criadan_cluster");
+	public static final RegistryKey<PlacedFeature> LARGE_CRIADAN = of("large_criadan");
+	public static final RegistryKey<PlacedFeature> POINTED_CRIADAN = of("pointed_criadan");
+
+	public static final RegistryKey<PlacedFeature> ANHYDRITE_CLUSTER = of("anhydrite_cluster");
+	public static final RegistryKey<PlacedFeature> LARGE_ANHYDRITE = of("large_anhydrite");
+	public static final RegistryKey<PlacedFeature> POINTED_ANHYDRITE = of("pointed_anhydrite");
+
+	public static final RegistryKey<PlacedFeature> GOLDEN_CLAY_POOL = of("golden_clay_pool");
+	public static final RegistryKey<PlacedFeature> GOLDEN_CLAY_PATCH = of("golden_clay_patch");
+
+	public static final RegistryKey<PlacedFeature> ACHREAN_MOSS_PATCH = of("archean_moss_patch");
+	public static final RegistryKey<PlacedFeature> ACHREAN_MOSS_CEILING = of("achrean_moss_ceiling");
+	public static final RegistryKey<PlacedFeature> ACHREAN_GOLDEN_CLAY = of("achrean_golden_clay");
+
+	private static RegistryKey<PlacedFeature> of(String path) {
+		return RegistryKey.of(RegistryKeys.PLACED_FEATURE, Archeon.createId(path));
+	}
+}
